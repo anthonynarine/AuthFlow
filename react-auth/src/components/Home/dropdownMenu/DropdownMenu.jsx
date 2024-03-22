cfbff7ecee1a213e643b5274b4c3e4668783c3a6
anthonynarine <anarine83@gmail.com>
2024-03-22 11:36:24 -0400

    updated the dropdown menu with react and djano icons and updated anchor position and bg color

--- react-auth/src/components/Home/dropdownMenu/DropdownMenu.jsx
+++ react-auth/src/components/Home/dropdownMenu/DropdownMenu.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./DropdownMenu.css";
+import { FaReact } from 'react-icons/fa'; // For the React icon
+import { DiPython } from 'react-icons/di'; // Example using a Python icon for Django
+
 
 export const DropdownMenu = ({ isOpen, toggleDropdownMenu }) => {
     return (
         <div className={`dropdown ${isOpen ? 'show' : ''}`}>
-            <button className="btn btn-secondary dropdown-toggle" 
+            <button className="inherit-color-bold-text" 
                     type="button" 
                     onClick={toggleDropdownMenu} 
                     aria-expanded={isOpen ? 'true' : 'false'}>
                 Features
             </button>
-            <ul className={`dropdown-menu ${isOpen ? 'show' : ''}`} >
-                <li><Link className="dropdown-item" to="/action">Action</Link></li>
-                <li><Link className="dropdown-item" to="/another-action">Another action</Link></li>
+            <ul className={`dropdown-menu ${isOpen ? 'show' : ''}`}>
+                <li><Link className="dropdown-item" to="/Features">
+                    <FaReact className='react-icon' /> React
+                    </Link></li>
+                <li><Link className="dropdown-item" to="/another-action">
+                    <DiPython className='django-icon' /> Django
+                    </Link></li>
             </ul>
         </div>
     );

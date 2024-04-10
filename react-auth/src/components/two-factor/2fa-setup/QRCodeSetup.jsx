0d86524e172113a7d522f125dc56db01a44de2f1
anthonynarine <anarine83@gmail.com>
2024-04-10 18:24:06 -0400

    updated QRCode style

--- react-auth/src/components/two-factor/2fa-setup/QRCodeSetup.jsx
+++ react-auth/src/components/two-factor/2fa-setup/QRCodeSetup.jsx
@@ -1,15 +1,19 @@
-import React, { useState} from "react"
+import React, { useEffect} from "react"
 import { useAuthServices } from "../../../context/auth/AuthContext"
-
+import "./QRCodeSetup.css"
 
 export const QRCodeSetup = () => {
 
+    const {fetchQRCode,qrCode,} = useAuthServices();  
+
+    useEffect(()=>{
+        fetchQRCode();
+    }, [fetchQRCode]);
 
     return(
-        <div>
-            <h1>
-                Under Construction 
-            </h1>
+        <div className="qr-container">
+            <h2 className="qr-title">Scan this QR Code with your Authenticator App</h2>
+            {qrCode && <img src={qrCode} alt="QR Code for 2FA Setup" className="qr-image" />}
         </div>
     )
 }

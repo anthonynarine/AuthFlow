47758207c0c07d4d98146441780794b130d01cf8
anthonynarine <anarine83@gmail.com>
2024-07-01 14:46:46 -0400

    updated the request to the gpt sever to now manage the client and server message history

--- react-auth/src/hooks/useGptRequest.jsx
+++ react-auth/src/hooks/useGptRequest.jsx
@@ -1,17 +1,27 @@
 import { useState, useCallback } from "react";
 import axios from "axios";
+import { useUserSessionServices } from "../context/auth/UserSessionContext";
 
 const useGptRequest = () => {
     const [gptResponse, setGptResponse] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const { user } = useUserSessionServices();
+    const [chatHistory, setChatHistory] = useState([]); 
+
 
     const requestToGpt = useCallback(async (prompt) => {
+        const sender = user ? user.first_name : "user" 
+        const userEntry = { sender, message: prompt };
+        setChatHistory([...chatHistory, userEntry]) // Update chat history with user entry
+
         setLoading(true);
         setError(null);
 
         try {
             const { data } = await axios.post('https://gait-gpt.herokuapp.com/api/gpt/', { prompt });
+            const aiEntry = { sender: "ai", message: data};
+            setChatHistory(prevHistory => [...prevHistory, aiEntry]); // Update chat history with ai entry
             setGptResponse(data);        
         } catch (error) {
             setError(error.response ? error.response.data : error.message);
@@ -20,7 +30,7 @@ const useGptRequest = () => {
         }
     }, []);
 
-    return { gptResponse, loading, error, requestToGpt}
+    return { gptResponse, loading, error, chatHistory, requestToGpt}
 };
 
 export default useGptRequest

4f6d061ce4b8c93bb010f0073e5c8554d6f81957
anthonynarine <anarine83@gmail.com>
2024-07-04 17:36:28 -0400

    updated scrollbar css for chat component

--- react-auth/src/components/footer/Footer.jsx
+++ react-auth/src/components/footer/Footer.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { FaGithub, FaLinkedin,  FaDatabase } from 'react-icons/fa';
-import './Footer.css'; 
+import { FaGithub, FaLinkedin, FaDatabase } from 'react-icons/fa';
+import './Footer.css';
 
 export const Footer = () => {
   return (
     <footer className="mt-auto text-white-50 footer-container">
-      <div className="link-container"> {/* Add this div */}
+      <div className="link-container">
         <a href="https://documenter.getpostman.com/view/23868442/2sA3XY6xgj" className="api-link" target="_blank" rel="noopener noreferrer">
-        <FaDatabase /> Api Documentation 
+          <FaDatabase /> <span>Api Documentation</span>
         </a>
         <a href="https://github.com/anthonynarine" target="_blank" rel="noopener noreferrer" className="github-link">
-          <FaGithub /> GitHub
+          <FaGithub /> <span>GitHub</span>
         </a>
         <a href="https://www.linkedin.com/in/anthony-narine-9ab567245/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
-          <FaLinkedin /> LinkedIn
+          <FaLinkedin /> <span>LinkedIn</span>
         </a>
       </div>
     </footer>
   );
 };
+
+export default Footer;
